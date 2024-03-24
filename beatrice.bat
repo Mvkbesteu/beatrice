91610c010ec6b2e6496f43176095dd4154434dc9
Mvk <53517019+Mvkbesteu@users.noreply.github.com>
2024-03-24 14:49:37 +0200

    asdsd

--- beatrice.bat
+++ beatrice.bat
@@ -27,16 +27,3 @@
 ::└───────────────────────────────────────────────┘ 𓇢𓆸
 ::  ₊˚⊹꒷ 
 
-@echo off
-title beatrice
-chcp 65001
-mode 93, 48
-for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a" & set "COL=%%b")
-goto admin.perms
-
-:admin.perms
-rem Code to request Admin permissions in order to continue  ~ ʀᴇǫᴜɪʀᴇᴅ 
-pause
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit) 
-:: ⚠️ ɴᴏᴛ ʀᴜɴɴɪɴɢ ʙᴇᴀᴛʀɪᴄᴇᴇ ᴀѕ ᴀᴅᴍɪɴɪѕᴛʀᴀᴛᴏʀ ᴄᴏᴜʟᴅ ʟᴇᴀᴅ ɪɴᴛᴏ ᴛʜᴇ ѕᴇᴛᴛɪɴɢѕ ɴᴏᴛ ᴀᴘᴘʟʏɪɴɢ ᴘʀᴏᴘᴇʀʟʏ.
-
