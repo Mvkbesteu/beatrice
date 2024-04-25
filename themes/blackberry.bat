fd87416a40626e598901cb7859702891addbd399
Mvk <53517019+Mvkbesteu@users.noreply.github.com>
2024-04-25 10:15:02 +0300

    Add files via upload

--- themes/blackberry.bat
+++ themes/blackberry.bat
@@ -1,70 +1,7 @@
 @echo off
-:: *ੈ✩‧₊ ⋆˚🐾˖° *ੈ✩‧₊ ⋆˚  
-::┌───────────────────────────────────────────────┐ 
-::                            
-::        _       __             _ _   
-::     __| | ___ / _| __ _ _   _| | |_                              
-::    / _` |/ _ \ |_ / _` | | | | | __|
-::   | (_| |  __/  _| (_| | |_| | | |_ 
-::    \__,_|\___|_|  \__,_|\__,_|_|\__|      Last Updated 3/26/2024                 
-::    • beatricee Default Theme 1.0                v1.0.0-BETA                                                                                                  
-::	 
-::└───────────────────────────────────────────────┘ 𓇢𓆸
-::  ₊˚⊹꒷      
-::
-:: Colors                
 set iris=[38;2;27;162;140m
 :: General Colors
 set primary=[38;2;18;31;91m
 set secondary=[38;2;56;54;64m
 set thirdry=[38;2;33;32;38m 
 set shading0=[38;2;255;255;255m
-set shading1=[38;2;170;169;168m
-set shading2=[38;2;85;84;84m
-set shading3=[38;2;45;44;44m
-set outline=[38;2;45;44;44m
-set new=[38;2;17;135;156m
-set success=[38;2;27;194;118m
-set warning=[38;2;157;181;26m
-set updated=[38;2;20;106;135m
-set error=[38;2;204;70;65m
-set reset=[0m
-set backround=[48;2;21;21;21m
-set backround7=[48;2;14;15;16m
-set backround2=[48;2;20;20;20m
-set backround3=[48;2;19;19;19m
-set backroundchng=[48;2;17;17;21m
-:: Roles
-set user=%secondary%User
-set media=[38;2;81;89;81mMedia 
-set tester=%success%Tester
-set moderator=%new%Moderator
-set admin=%new%Admin
-set developer=%new%Dev
-::logo
-set iris1=[38;2;96;221;206m
-set iris2=[38;2;84;212;199m
-set iris3=[38;2;74;202;193m
-set iris4=[38;2;71;198;186m
-set iris5=[38;2;62;192;174m
-set iris6=[38;2;56;184;161m
-set iris7=[38;2;44;176;151m
-set iris8=[38;2;37;172;150m
-set iris9=[38;2;27;162;140m
-:: Credentials
-set gatherer=[38;2;39;140;134m
-set manager=[38;2;48;127;125m
-:: Effects
-set blink=[5m
-set account.blink.off=%shading2%
-set account.blink.on=%secondary%%blink%
-set build=v%secondary%1.0.0%shading1%-%secondary%BETA
-:: Stages
-set setup.stages=17
-
-::Emojies
-set emoji.error=☠️
-set emoji.success=
-set emoji.installer=%emoji.welcome%
-set emoji.welcome=👋
-set emoji.note=📑
