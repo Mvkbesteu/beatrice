58b5da9183c39b0f11c7b7a8bc03b5ffab7f9c73
Mvk <53517019+Mvkbesteu@users.noreply.github.com>
2024-04-27 21:53:35 +0300

    Add files via upload

--- beatrice.bat
+++ beatrice.bat
@@ -22,7 +22,7 @@
 ::      | |_) | (_| | |_) | |   
 ::      |_.__/ \__,_| .__/|_|   
 ::                  |_|                           Last Updated 3/24/2024
-::    • beatricee Administrator Perms Request          v0.7.3-BETA    
+::    • beatrice Administrator Perms Request          v0.7.3-BETA    
 ::
 ::└───────────────────────────────────────────────┘ 𓇢𓆸
 ::  ₊˚⊹꒷ 
@@ -39,18 +39,74 @@ if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
 :: ⚠️ ɴᴏᴛ ʀᴜɴɴɪɴɢ ʙᴇᴀᴛʀɪᴄᴇᴇ ᴀѕ ᴀᴅᴍɪɴɪѕᴛʀᴀᴛᴏʀ ᴄᴏᴜʟᴅ ʟᴇᴀᴅ ɪɴᴛᴏ ᴛʜᴇ ѕᴇᴛᴛɪɴɢѕ ɴᴏᴛ ᴀᴘᴘʟʏɪɴɢ ᴘʀᴏᴘᴇʀʟʏ.
 
 
-:: Checks if beatricee has ben ran before on this system.
+:: Checks if beatrice has ben ran before on this system.
 :old.check
->nul 2>&1 call %systemdrive%\beatricee\config\core\checks\oldver.bat
+>nul 2>&1 call %systemdrive%\beatrice\config\core\checks\oldver.bat
 if "%firstlaunch%" equ "true" (
-    goto cli
+    goto loader
 ) ELSE (
-    goto dependency
+    goto check.windows
 )
+
+:: *ੈ✩‧₊ ⋆˚🐾˖° *ੈ✩‧₊ ⋆˚  
+::┌───────────────────────────────────────────────┐ 
+::                            
+::      
+::            _                _               _    
+::  __      _(_)_ __       ___| |__   ___  ___| | __
+::  \ \ /\ / / | '_ \     / __| '_ \ / _ \/ __| |/ /
+::   \ V  V /| | | | |_  | (__| | | |  __/ (__|   < 
+::    \_/\_/ |_|_| |_(_)  \___|_| |_|\___|\___|_|\_\                              
+::                                        Last Updated 3/24/2024
+::    • beatrice Windows Version Check          v0.7.3-BETA    
+::
+::└───────────────────────────────────────────────┘ 𓇢𓆸
+::  ₊˚⊹꒷ 
+:: 
+:: 🫐 ᴛʜᴇ ᴄᴏᴅᴇ ʙᴇʟᴏᴡ ᴅᴇᴛᴇʀᴍɪɴᴇѕ ᴀɴᴅ ᴅᴇᴛᴇᴄᴛѕ ᴛʜᴇ ᴠᴇʀѕɪᴏɴ ᴏꜰ ᴡɪɴᴅᴏᴡѕ ʏᴏᴜ᾽ʀᴇ ʀᴜɴɴɪɴɢ.
+::   ɪꜰ ɪᴛ ɪѕ ʙᴇʟᴏᴡ  ɪᴛ ᴡᴏɴ᾽ᴛ ʟᴇᴛ ʏᴏᴜ ɪɴ, ᴅᴏ ɴᴏᴛ ᴇᴅɪᴛ ᴛʜɪѕ ʏᴏᴜ ᴡɪʟʟ ʙʀᴇᴀᴋ ᴛʜᴇ ѕᴏꜰᴛᴡᴀʀᴇ.
+
+:check.windows
+cls
+set verString="Microsoft Windows [Version 39.0.99999]"
+REM set verString="Microsoft Windows [Version 10.0.22621]"
+REM set verString="Microsoft Windows [Version 10.0.22000]"
+REM set verString="Microsoft Windows [Version 10.0.10240]"
+REM set verString="Microsoft Windows [Version 6.3.9600]"
+REM set verString="Microsoft Windows [Version 6.1.7601]"
+REM set verString="Microsoft Windows XP [Version 5.1.2600]"
+REM set verString="Microsoft Windows [Version 4.10]"
+for /f "tokens=4-7 delims=[.] " %%i in (%verString%) do @(if %%i==Version (set os_ver_org=%%j.%%k) else (if %%i geq 10 (set os_ver_org=%%i.%%j.%%k) else (set os_ver_org=%%i.%%j)))
+set os_ver=%os_ver_org%
+if %os_ver_org:~0,1% gtr 3 set os_ver=0%os_ver_org%
+if %os_ver% GEQ 10.0.5325000 set osName=Windows 11 & goto dependency
+if %os_ver% GEQ 10.0.10240 if %os_ver% lss 10.0.22000 set osName=Windows 10& goto windows.unsupported
+if %os_ver% equ 06.3 set osName=Windows 8.1 & goto windows.unsupported
+if %os_ver% equ 06.2 set osName=Windows 8  & goto windows.unsupported
+if %os_ver% equ 06.1 set osName=Windows 7  & goto windows.unsupported
+if %os_ver% equ 06.0 set osName=Windows Vista  & goto windows.unsupported
+if %os_ver% equ 05.2 set osName=Windows XP x64 or Server 2003  & goto windows.unsupported
+if %os_ver% equ 05.1 set osName=Windows XP  & goto windows.unsupported
+if %os_ver% equ 05.0 set osName=Windows 2000  & goto windows.unsupported
+if %os_ver% lss 05.0 set osName=Windows ME or 98 or less  & goto windows.unsupported
+Set osName=%shading1%a %secondary%Unknown Windows
+goto windows.unsupported
+
+:windows.unsupported
+mode 63, 12
+cls
+echo. 
+echo.                  [38;2;97;93;104m%osName% - Is Unsupported
+echo.[38;2;57;53;74m───────────────────────────────────────────────────────────────
+echo.
+echo.                  [38;2;117;113;124mPlease [5mupgrade[0m [38;2;117;113;124mto Windows 11
+echo.                  [38;2;127;123;134mIn order to use [4mbeatrice.zip[0m
+pause>nul
+exit /b
+
 
 
 :dependency
-
 :: *ੈ✩‧₊ ⋆˚🐾˖° *ੈ✩‧₊ ⋆˚  
 ::┌───────────────────────────────────────────────┐ 
 ::                            
@@ -59,7 +115,7 @@ if "%firstlaunch%" equ "true" (
 ::     | | '_ \/ __| __/ _` | | |/ _ \ '__|
 ::     | | | | \__ \ || (_| | | |  __/ |   
 ::     |_|_| |_|___/\__\__,_|_|_|\___|_|     Last Updated 3/26/2024                 
-::    • beatricee Dependencies Installer  1.0                v1.0.0-BETA                                                                                                  
+::    • beatrice Dependencies Installer  1.0       v1.0.0-BETA                                                                                                  
 ::	 
 ::└───────────────────────────────────────────────┘ 𓇢𓆸
 ::  ₊˚⊹꒷      
@@ -71,25 +127,22 @@ if "%firstlaunch%" equ "true" (
 :: Do not skip this process, or else the program won't run.
 ::
 :: ᴄʀᴇᴀᴛᴇѕ ᴄᴏɴꜰɪɢ ᴅɪʀᴇᴄᴛᴏʀʏ
-mkdir %systemdrive%\beatricee\config\themes >nul 2>&1
-
-:: ᴄʀᴇᴀᴛᴇѕ ᴄᴏɴꜰɪɢ ⁄ ᴄᴏʀᴇ / ᴄʜᴇᴄᴋѕ
-mkdir %systemdrive%\beatricee\config\core\checks\ >nul 2>&1
+mkdir %systemdrive%\beatrice\config\themes >nul 2>&1
 
 :: ᴄʀᴇᴀᴛᴇѕ ᴀѕѕᴇᴛѕ ᴅɪʀᴇᴄᴛᴏʀʏ
-mkdir %systemdrive%\beatricee\assets >nul 2>&1
-goto yer
+mkdir %systemdrive%\beatrice\assets >nul 2>&1
+goto theme.installation
 
 
 
-:yer
+:theme.installation
 REM Set the URL of your .bat file
-set "default.theme=https://raw.githubusercontent.com/Mvkbesteu/beatricee/main/themes/default.bat"
-set "nature.theme=https://raw.githubusercontent.com/Mvkbesteu/beatricee/main/themes/nature.bat"
-set "blackberry.theme=https://raw.githubusercontent.com/Mvkbesteu/beatricee/main/themes/blackberry.bat"
+set "default.theme=https://raw.githubusercontent.com/Mvkbesteu/beatrice/main/themes/default.bat"
+set "nature.theme=https://raw.githubusercontent.com/Mvkbesteu/beatrice/main/themes/nature.bat"
+set "blackberry.theme=https://raw.githubusercontent.com/Mvkbesteu/beatrice/main/themes/blackberry.bat"
 
 REM Set the target directory for installation
-set "target_dir=%systemdrive%\beatricee\config\themes\"
+set "target_dir=%systemdrive%\beatrice\config\themes\"
 
 REM Create the target directory if it doesn't exist (silent)
 if not exist "%target_dir%" mkdir "%target_dir%" > NUL
@@ -119,7 +172,7 @@ exit
 ::     \ V /  __/ |  \__ \ | (_) | | | | | (__| | | |  __/ (__|   < 
 ::      \_/ \___|_|  |___/_|\___/|_| |_|  \___|_| |_|\___|\___|_|\_\                                                            
 ::                                           Last Updated 4/25/2024                 
-::    • beatricee Update Checker 1.0                v1.0.0-BETA                                                                                                  
+::    • beatrice Update Checker 1.0                v1.0.0-BETA                                                                                                  
 ::	 
 ::└───────────────────────────────────────────────┘ 𓇢𓆸
 ::  ₊˚⊹꒷      
@@ -139,15 +192,15 @@ if exist "%TEMP%\ver.bat" (
 )
 
 REM Download a file named "ver.bat" from the specified URL and save it to the TEMP directory
-curl -g -L -# -o "%TEMP%\ver.bat" "https://raw.githubusercontent.com/Mvkbesteu/beatricee/main/config/version/latest.cpp" >nul 2>&1
+curl -g -L -# -o "%TEMP%\ver.bat" "https://raw.githubusercontent.com/Mvkbesteu/beatrice/main/config/version/latest.cpp" >nul 2>&1
 
 REM Execute the downloaded "ver.bat" file
 call "%TEMP%\ver.bat"
 6
 REM Compare the values of local and localtwo variables to check if they are equal
 if "%localtwo%"=="%local%" (
-    REM If the values are equal, delete the downloaded "ver.bat" file and go to the "authentification" label
-    del "%TEMP%\ver.bat" & goto authentification
+    REM If the values are equal, delete the downloaded "ver.bat" file and go to the "succeded" label
+    del "%TEMP%\ver.bat" & goto succeded
 ) else (
     REM If the values are not equal, delete the downloaded "ver.bat" file and go to the "update.found" label
     del "%TEMP%\ver.bat" & goto update.found
@@ -167,7 +220,7 @@ if "%localtwo%"=="%local%" (
 ::      \__,_| .__/ \__,_|\__,_|\__\___(_)
 ::           |_|                                                                                                   
 ::                                           Last Updated 4/25/2024                 
-::    • beatricee Update Checker 1.0                v1.0.0-BETA                                                                                                  
+::    • beatrice Update Checker 1.0                v1.0.0-BETA                                                                                                  
 ::	 
 ::└───────────────────────────────────────────────┘ 𓇢𓆸
 ::  ₊˚⊹꒷      
@@ -213,15 +266,15 @@ echo.
 echo.
 echo.
 echo.
-echo.     %secondary%╭──%secondary%[%secondary%root%secondary%@%primary%beatricee%secondary%]
+echo.     %secondary%╭──%secondary%[%secondary%root%secondary%@%primary%beatrice%secondary%]
 set /p "choice=%DEL%     ╰────➤ "  
 :: Check if the user input is empty
 if /i "%choice%"=="" (
     :: If input is empty, open the default browser and navigate to the specified GitHub repository URL
-    start https://github.com/Mvkbesteu/beatricee/releases
+    start https://github.com/Mvkbesteu/beatrice/releases
 ) else (
     :: If input is not empty, open the default browser and navigate to the specified GitHub repository URL
-    start https://github.com/Mvkbesteu/beatricee/releases
+    start https://github.com/Mvkbesteu/beatrice/releases
 )
 goto update.found
 
@@ -234,14 +287,34 @@ goto update.found
 ::     | | (_) | (_| | (_| |  __/ |   
 ::     |_|\___/ \__,_|\__,_|\___|_|   
 ::                                           Last Updated 3/26/2024                 
-::    • beatricee Loader 1.0                v1.0.0-BETA                                                                                                  
+::    • beatrice Loader 1.0                v1.0.0-BETA                                                                                                  
 ::	 
 ::└───────────────────────────────────────────────┘ 𓇢𓆸
 ::  ₊˚⊹꒷      
 ::
 
 
-:Loader
+:: *ੈ✩‧₊ ⋆˚🐾˖° *ੈ✩‧₊ ⋆˚  
+::┌───────────────────────────────────────────────┐ 
+::                            
+::                _               
+::       ___  ___| |_ _   _ _ __  
+::      / __|/ _ \ __| | | | '_ \ 
+::      \__ \  __/ |_| |_| | |_) |
+::      |___/\___|\__|\__,_| .__/ 
+::                         |_|    
+::                                    Last Updated 4/27/2024                 
+::    • beatrice Setup Start-up 1.0                v1.0.0-BETA                                                                                                  
+::	 
+::└───────────────────────────────────────────────┘ 𓇢𓆸
+::  ₊˚⊹꒷      
+::
+:: ɪɴɪᴛɪᴀʟ ѕᴛᴀʀᴛ ᴏꜰ ᴛʜᴇ ɪɴѕᴛᴀʟʟᴀᴛɪᴏɴ ᴘʀᴏᴄᴇѕѕ ꜰᴏʀ ʙᴇᴀᴛʀɪᴄᴇ.
+:: 
+
+
+
+:setup
 cls
 echo.[48;2;17;17;26m                                                                                             %reset%                                 
 echo.        %primary%ཻུ۪۪♡.%reset%                       %primary%♪%reset%                               %primary%✎%reset%                   
@@ -272,18 +345,13 @@ echo.
 echo.                                        %emoji.note%
 echo.                    %outline%⭒ ༺☽─────────────%shading1% [48;2;21;21;21m %shading1%ɴᴏᴛᴇ [0m%outline% ─────────────☾༻ ⭒
 echo.
-echo.                    %outline%• %secondary%Beatricee %shading1%is still in %backround2% %secondary%beta %shading1%access.%reset%
+echo.                    %outline%• %secondary%beatrice %shading1%is still in %backround2% %secondary%beta %shading1%access.%reset%
 echo.                    %outline%• %shading1%Please %secondary%report %shading1%any %secondary%bugs %shading1%if you've found.
 echo.    
 echo.    
 echo.
 echo.
 echo.
-echo.     %secondary%╭──%secondary%[%secondary%root%secondary%@%primary%beatricee%secondary%]
+echo.     %secondary%╭──%secondary%[%secondary%root%secondary%@%primary%beatrice%secondary%]
 set /p "choice=%DEL%     ╰────➤ "  
 goto authentication
-
-:cli
-cls
-echo. yo
-pause
