6f4a03fb85ffc69ef648514587c03cc686dff593
Mvk <53517019+Mvkbesteu@users.noreply.github.com>
2024-12-13 19:33:09 +0200

    Update default.bat

--- themes/default.bat
+++ themes/default.bat
@@ -15,7 +15,7 @@
 :: Colors                
 set iris=[38;2;255;162;140m
 :: General Colors
-set primary=[38;2;10;79;240m
+set primary=[38;2;41;94;217m
 set secondary=[38;2;56;54;64m
 set thirdry=[38;2;33;32;38m 
 set shading0=[38;2;255;255;255m
